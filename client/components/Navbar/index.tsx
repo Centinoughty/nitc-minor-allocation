9f1f81993c12afd5b57f6a5665b019ca0fc86a58
jasijasil <mohammedjass007@gmail.com>
2025-10-11 14:59:34 +0530

    console .log deleted did som e corrections

--- client/components/Navbar/index.tsx
+++ client/components/Navbar/index.tsx
@@ -104,17 +104,17 @@ export default function Navbar() {
                     <ul className="flex flex-col font-medium mt-4 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-transparent dark:bg-gray-800 md:dark:bg-transparent dark:border-gray-700">
                         <li>
                             <Link
-                                href="/courses"
-                                className="block py-2 px-3 md:p-0 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"                            >
-                                Courses
-                            </Link>
-                        </li>
-                        <li>
-                            <Link
                                 href="/Dashboard"
                                 className="block py-2 px-3 md:p-0 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
                             >
                                 Dashboard
+                            </Link>
+                        </li>
+                        <li>
+                            <Link
+                                href="/courses"
+                                className="block py-2 px-3 md:p-0 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"                            >
+                                Courses
                             </Link>
                         </li>
                         <li>
