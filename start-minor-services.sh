4afc65937c4ea90c8656eb253edf5584e146c307
Nadeem M Siyam <nadeemvadasseril@gmail.com>
2025-10-11 14:17:14 +0530

    created script to run code on tmux

---
+++ start-minor-services.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+tmux new-session -d -s minor
+
+tmux rename-window -t minor:0 'server'
+tmux send-keys -t minor:0 "cd $PWD/server && nodemon" C-m
+
+tmux new-window -t minor:1 -n 'client'
+tmux send-keys -t minor:1 "cd $PWD/client && npm start" C-m
+
+tmux new-window -t minor:2 -n 'admin'
+tmux send-keys -t minor:2 "cd $PWD/admin && serve -s dist -p 3002" C-m
+
+tmux attach -t minor
